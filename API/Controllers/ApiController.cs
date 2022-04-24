74a32d974ae65534d7295771e96a12430c7f8541
Guilherme Roger <guilhermerogerbergamini@gmail.com>
2022-04-24 17:08:00 -0300

    Senha string de conexão alterada

--- API/Controllers/ApiController.cs
+++ API/Controllers/ApiController.cs
@@ -14,7 +14,7 @@ namespace API.Controllers
       [HttpPost("Singin")]
       public UserData GetLogin(UserLogin dataUser)
       {
-            string StringCon = "Server=localhost;Database=usuariosdb;Uid=root;Pwd=*****;";
+            string StringCon = "Server=localhost;Database=usuariosdb;Uid=root;Pwd=Guizinho@2408;";
             var con = new MySqlConnection(StringCon);           
 
             con.Open();
